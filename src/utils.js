0f9f5d0c679e1fd69b1d40992545909b870be50b
Shreyash Raj <58896906+Shashank3736@users.noreply.github.com>
2021-10-17 16:03:23 +0530

    Update utils.js
    Solve bug in #378

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const validateEmbedColor = (embedColor) => {
     try {
-        Discord.Util.resolveColor(embedColor);
+        embedColor = Discord.Util.resolveColor(embedColor);
         if (!isNaN(embedColor) && typeof embedColor === 'number') return true;
         else return false;
     } catch {
